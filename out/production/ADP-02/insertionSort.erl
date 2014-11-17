1f7b6ec624351c4940b41548d3738ca5a31a1e81
Isa <louisa@spahl-hamburg.de>
2014-11-17 19:46:00 +0100

    insertionSort Algorithmus noch nicht getestet...kommt morgen :)

--- out/production/ADP-02/insertionSort.erl
+++ out/production/ADP-02/insertionSort.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @author KamikazeOnRoad
+%%% @author Marjan und Louisa
 %%% @copyright (C) 2014, <COMPANY>
 %%% @doc
 %%%
@@ -7,7 +7,69 @@
 %%% Created : 16. Nov 2014 18:48
 %%%-------------------------------------------------------------------
 -module(insertionSort).
--author("KamikazeOnRoad").
 
 %% API
--export([]).
+-export([insertionS/3]).
+
+
+
+%% Wahrheitstafel:
+%% __A___B____A_v_B__
+%% | 0   0 |    0   |
+%% | 0   1 |    1   |
+%% | 1   0 |    1   |
+%% |_1___1_|____1___|
+%%
+
+
+
+
+insertionS(Array, Von, Bis) ->
+  ReturnList = arrayS:initA(),
+  insertionS(Array, ReturnList, Von, Bis, false ).
+
+%insertionS({First,Second, Rest}, ReturnList, Von, Bis, 1) when First < Second and Rest == {} ->
+
+%    insertionS(ReturnList, {}, Von, Bis, 1);
+
+
+
+
+%% Algorithmus ist mit dem gesamten tauschen fertig,
+%% im letzten Durchgang wurde nicht mehr getauscht.
+insertionS({}, ReturnList, _Von, _Bis, false) ->
+  ReturnList;
+
+
+
+%% Algorithmus ist noch nicht fertig mit tauschen,
+%% beim Durchgehen der Liste im letzten Durchgang wurde immernoch getauscht.
+insertionS({}, ReturnList, _Von, _Bis, true) ->
+  ReturnListNew = arrayS:initA(),
+  insertionS(ReturnList, ReturnListNew, _Von, _Bis, false);
+
+
+
+%% Das sich erste anschauende Elemente ist kleiner als das Zweite.
+%% Es muss nicht getauscht werden.
+insertionS({First, Second, Rest}, ReturnList, _Von, _Bis, Switched) when First < Second ->
+  PosForFirst = arrayS:lengthA(ReturnList),
+  NewArray1 = arrayS:setA(ReturnList, PosForFirst, First),
+  NewSwitched = (Switched and false),
+  insertionS({Second, Rest}, NewArray1, _Von, _Bis, NewSwitched);
+
+
+
+%% Das sich erste anschauende Elemente ist großer als das Zweite.
+%% Es muss getauscht werden.
+insertionS({First, Second, Rest}, ReturnList, _Von, _Bis, Switched) when First > Second ->
+  PosForSecond = arrayS:lengthA(ReturnList),
+  NewArray1 = arrayS:setA(ReturnList, PosForSecond, Second),
+  NewSwitched = (Switched and true),
+  insertionS({First, Rest}, NewArray1, _Von, _Bis, NewSwitched).
+
+
+
+
+
+
--- src/insertionSort.erl
+++ src/insertionSort.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @author KamikazeOnRoad
+%%% @author Marjan und Louisa
 %%% @copyright (C) 2014, <COMPANY>
 %%% @doc
 %%%
@@ -7,7 +7,68 @@
 %%% Created : 16. Nov 2014 18:48
 %%%-------------------------------------------------------------------
 -module(insertionSort).
--author("KamikazeOnRoad").
 
 %% API
--export([]).
+-export([insertionS/3]).
+
+
+
+%% Wahrheitstafel:
+%% __A___B____A_v_B__
+%% | 0   0 |    0   |
+%% | 0   1 |    1   |
+%% | 1   0 |    1   |
+%% |_1___1_|____1___|
+%%
+
+
+
+
+insertionS(Array, Von, Bis) ->
+  ReturnList = arrayS:initA(),
+  insertionS(Array, ReturnList, Von, Bis, false ).
+
+
+
+%% Algorithmus ist mit dem gesamten tauschen fertig,
+%% im letzten Durchgang wurde nicht mehr getauscht.
+insertionS({First}, ReturnList, _Von, _Bis, false) ->
+  Pos = arrayS:lengthA(ReturnList),
+  arrayS:setA(ReturnList,Pos, First),
+  ReturnList;
+
+
+
+%% Algorithmus ist noch nicht fertig mit tauschen,
+%% beim Durchgehen der Liste im letzten Durchgang wurde immernoch getauscht.
+insertionS({First}, ReturnList, _Von, _Bis, true) ->
+  ReturnListNew = arrayS:initA(),
+  Pos = arrayS:lengthA(ReturnList),
+  arrayS:setA(ReturnList,Pos, First),
+  insertionS(ReturnList, ReturnListNew, _Von, _Bis, false);
+
+
+
+%% Das sich erste anschauende Elemente ist kleiner als das Zweite.
+%% Es muss nicht getauscht werden.
+insertionS({First, Second, Rest}, ReturnList, _Von, _Bis, Switched) when First < Second ->
+  PosForFirst = arrayS:lengthA(ReturnList),
+  NewArray1 = arrayS:setA(ReturnList, PosForFirst, First),
+  NewSwitched = (Switched and false),
+  insertionS({Second, Rest}, NewArray1, _Von, _Bis, NewSwitched);
+
+
+
+%% Das sich erste anschauende Elemente ist großer als das Zweite.
+%% Es muss getauscht werden.
+insertionS({First, Second, Rest}, ReturnList, _Von, _Bis, Switched) when First > Second ->
+  PosForSecond = arrayS:lengthA(ReturnList),
+  NewArray1 = arrayS:setA(ReturnList, PosForSecond, Second),
+  NewSwitched = (Switched and true),
+  insertionS({First, Rest}, NewArray1, _Von, _Bis, NewSwitched).
+
+
+
+
+
+
