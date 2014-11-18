043f5326df5b6fa709d3952278f75dee460e5620
Isa <louisa@spahl-hamburg.de>
2014-11-19 00:40:55 +0100

    Algorithmus läuft

--- src/insertionSort.erl
+++ src/insertionSort.erl
@@ -9,7 +9,7 @@
 -module(insertionSort).
 
 %% API
--export([insertionS/3]).
+-export([insertionS/3, unsortedFront/3, sortedPart/5, unsortedEnd/3, concatTwoArray/2]).
 
 
 
@@ -22,11 +22,23 @@
 %%
 
 
-
-
 insertionS(Array, Von, Bis) ->
-  ReturnList = arrayS:initA(),
-  insertionS(Array, ReturnList, Von, Bis, false ).
+%%   Length = arrayS:lengthA(Array),
+%%   if
+%%     (Length >= Bis) ->
+      List = arrayS:initA(),
+      ArrayNotToSort1 = unsortedFront(Array, List, Von), %% gibt den ersten Teil zurück, der nicht mit sortiert werden soll
+      ArrayToSort = sortedPart(Array, List, 0, Von, Bis),    %% gibt den zu sortierenden Teil zurück.
+      ArrayNotToSort2 = unsortedEnd(Array, List, Bis+1),
+      %% Algorithmus ausführen
+      NewSortedArray = insertionS(ArrayToSort,List, Von, Bis, false),
+      %% Konkatinieren zur Rückgabeliste
+      ArrayConcat = concatTwoArray(ArrayNotToSort1, NewSortedArray),
+      concatTwoArray(ArrayConcat, ArrayNotToSort2).
+%%     true ->
+%%       erlang:error("Die Obere Grenze der zu sortierende Liste befindet sich nicht mehr im Array")
+%%   end.
+
 
 
 
@@ -38,7 +50,6 @@ insertionS({First,{}}, ReturnList, _Von, _Bis, false) ->
   ReturnList2;
 
 
-
 %% Algorithmus ist noch nicht fertig mit tauschen,
 %% beim Durchgehen der Liste im letzten Durchgang wurde immernoch getauscht.
 insertionS({First,{}}, ReturnList, _Von, _Bis, true) ->
@@ -48,7 +59,6 @@ insertionS({First,{}}, ReturnList, _Von, _Bis, true) ->
   insertionS(ReturnList2, ReturnListNew, _Von, _Bis, false);
 
 
-
 %% Das sich erste anschauende Elemente ist kleiner als das Zweite.
 %% Es muss nicht getauscht werden.
 insertionS({First, {Second, Rest}}, ReturnList, _Von, _Bis, Switched) when First < Second ->
@@ -56,7 +66,6 @@ insertionS({First, {Second, Rest}}, ReturnList, _Von, _Bis, Switched) when First
   NewArray1 = arrayS:setA(ReturnList, PosForFirst, First),
   NewSwitched = (Switched or false),
   insertionS({Second, Rest}, NewArray1, _Von, _Bis, NewSwitched);
-
 
 
 %% Das sich erste anschauende Elemente ist großer als das Zweite.
@@ -71,6 +80,95 @@ insertionS({First, {Second, Rest}}, ReturnList, _Von, _Bis, Switched) when First
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%   HILFSFUNKTIONEN  %%
+%%     - unsortedFront/3
+%%     - sortedPart/5
+%%     - unsortedEnd/3
+%%     - concatTwoArray/2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Array x NewArray x PosAt x BisIndex -> NewArray
+%% Gibt den nicht zu sortierenden Teil aus der vor dem zu sortierenden Teil steht
+unsortedFront(Array, NewArray,  BisIndex) ->
+  unsortedFront(Array, NewArray, 0,  BisIndex).
+
+unsortedFront(_Array, NewArray, _PosAt,  BisIndex) when BisIndex == 0 ->
+  NewArray;
+
+unsortedFront(Array, NewArray, PosAt,  BisIndex) ->
+  Elem = arrayS:getA(Array, PosAt),
+  Pos = arrayS:lengthA(NewArray),
+  NewArray1 = arrayS:setA(NewArray, Pos, Elem),
+  NewPosAt = PosAt + 1,
+  NewBisIndex = BisIndex - 1,
+  unsortedFront(Array, NewArray1, NewPosAt, NewBisIndex).
+
+
+
+
+%% Array x NewArray x PosAt x VonIndex x BisIndex -> NewArray
+%% Gibt den zu sortierenden Teil aus
+ sortedPart(_Array, NewArray, PosAt, _VonIndex, BisIndex) when PosAt > BisIndex ->
+  NewArray;
+
+%% Die Liste wird durchgegangen ohne etwas zu machen,
+%% bis der Index gleich dem übergebenen Index VonIndex ist
+ sortedPart(Array, NewArray, PosAt, VonIndex, BisIndex) when PosAt < VonIndex ->
+   NewPosAt = PosAt + 1,
+   sortedPart(Array, NewArray, NewPosAt, VonIndex, BisIndex);
+
+ sortedPart(Array, NewArray, PosAt, VonIndex, BisIndex) when PosAt >= VonIndex ->
+   Elem = arrayS:getA(Array, PosAt),
+   Pos = arrayS:lengthA(NewArray),
+   NewArray1 = arrayS:setA(NewArray, Pos, Elem),
+   NewPosAt = PosAt + 1,
+   sortedPart(Array, NewArray1, NewPosAt, VonIndex, BisIndex).
+
+
+
+%% unsortedEnd: Array x Array x Startindex -> Array
+%% Gibt den Endteil des Arrays aus was nicht sortiert werden soll
+unsortedEnd(Array, NewArray, VonIndex) ->
+   unsortedEnd(Array, NewArray, 0, VonIndex).
+
+unsortedEnd(Array, NewArray, PosAt, VonIndex) when PosAt < VonIndex ->
+    NewPosAt = PosAt + 1,
+    unsortedEnd(Array, NewArray, NewPosAt, VonIndex);
+
+unsortedEnd(Array, NewArray, PosAt, VonIndex)  ->
+  Length = arrayS:lengthA(Array),
+  if
+    (PosAt < Length) ->
+       Elem = arrayS:getA(Array, PosAt),
+       Pos = arrayS:lengthA(NewArray),
+       NewArray1 = arrayS:setA(NewArray, Pos, Elem),
+       NewPosAt = PosAt + 1,
+       unsortedEnd(Array, NewArray1, NewPosAt, VonIndex);
+    true ->
+       NewArray
+  end.
+
+
+
+
+
+%% concatTwoArray: Array x Array -> Array
+%% Konkatiniert zwei Arrays
+ concatTwoArray(Array1, Array2) ->
+    concatTwoArray(Array1, Array2, 0).
+
+ concatTwoArray(Array1, Array2, PosAccu) ->
+    Pos1 = arrayS:lengthA(Array2),
+    if
+      (Pos1 > PosAccu) ->
+          Pos2 = arrayS:lengthA(Array1),
+          Elem = arrayS:getA(Array2, PosAccu),
+          NewArray = arrayS:setA(Array1, Pos2, Elem ),
+          concatTwoArray(NewArray, Array2, PosAccu+1);
+      (Pos1 =< PosAccu) ->
+          Array1
+    end.
 
 
 
---
+++ src/insertionSortTest.erl
@@ -0,0 +1,68 @@
+%%%-------------------------------------------------------------------
+%%% @author louisa
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 18. Nov 2014 15:16
+%%%-------------------------------------------------------------------
+-module(insertionSortTest).
+-author("louisa").
+
+-include_lib("eunit/include/eunit.hrl").
+
+insertionS_test() ->
+  A1 = arrayS:initA(),
+  A2 = arrayS:setA(A1,0,3),
+  A3 = arrayS:setA(A2,1,98),
+  A4 = arrayS:setA(A3,2,41),
+  A5 = arrayS:setA(A4,3,4),
+  A6 = arrayS:setA(A5,4,8),
+  A7 = arrayS:setA(A6,5,5),
+  ?assertEqual({3,{4,{5,{8,{41,{98,{}}}}}}}, insertionSort:insertionS(A7,0,5)),
+  ?assertEqual({3,{4,{41,{98,{8,{5,{}}}}}}}, insertionSort:insertionS(A7,1,3)).
+%%   ?assertError({}, insertionSort:insertionS(A7,0,6)).
+
+unsortedFront_test() ->
+  A1 = arrayS:initA(),
+  A2 = arrayS:setA(A1,0,3),
+  A3 = arrayS:setA(A2,1,98),
+  A4 = arrayS:setA(A3,2,41),
+  A5 = arrayS:setA(A4,3,4),
+  A6 = arrayS:setA(A5,4,8),
+  A7 = arrayS:setA(A6,5,5),
+  ?assertEqual({3,{98,{41,{}}}}, insertionSort:unsortedFront(A7, {},  3)),
+  ?assertEqual({}, insertionSort:unsortedFront(A7, {},  0)),
+  ?assertEqual({3,{98,{41,{4,{8,{5,{}}}}}}}, insertionSort:unsortedFront(A7, {},  6)).
+
+concatTwoArray_test() ->
+  A1 = arrayS:initA(),
+  A2 = arrayS:setA(A1,0,3),
+  A3 = arrayS:setA(A2,1,98),
+  A4 = arrayS:setA(A3,2,41),
+  A5 = arrayS:setA(A4,3,4),
+  A6 = arrayS:setA(A5,4,8),
+  A7 = arrayS:setA(A6,5,5),
+  ?assertEqual({3,{98,{41,{4,{8,{5,{11,{22,{33,{}}}}}}}}}}, insertionSort:concatTwoArray(A7, {11,{22,{33,{}}}})).
+
+sortedPart_test() ->
+  A1 = arrayS:initA(),
+  A2 = arrayS:setA(A1,0,3),
+  A3 = arrayS:setA(A2,1,98),
+  A4 = arrayS:setA(A3,2,41),
+  A5 = arrayS:setA(A4,3,4),
+  A6 = arrayS:setA(A5,4,8),
+  A7 = arrayS:setA(A6,5,5),
+  ?assertEqual({41,{4,{8,{}}}}, insertionSort:sortedPart(A7, {}, 0, 2, 4)).
+
+unsortedEnd_test() ->
+  A1 = arrayS:initA(),
+  A2 = arrayS:setA(A1,0,3),
+  A3 = arrayS:setA(A2,1,98),
+  A4 = arrayS:setA(A3,2,41),
+  A5 = arrayS:setA(A4,3,4),
+  A6 = arrayS:setA(A5,4,8),
+  A7 = arrayS:setA(A6,5,5),
+  ?assertEqual({8,{5,{}}}, insertionSort:unsortedEnd(A7, {}, 4)).
+
+
