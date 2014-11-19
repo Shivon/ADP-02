d9c64c2bb5b036c47c83206827c9bea28693cbf6
Marjan Bachtiari <marjan.bachtiari@haw-hamburg.de>
2014-11-19 01:00:37 +0100

    Update

--- src/selectionSort.erl
+++ src/selectionSort.erl
@@ -10,6 +10,34 @@
 -author("KamikazeOnRoad").
 
 %% API
-%%-export([selectionS/3]).
+-export([selectionS/3]).
+-import(arrayS, [initA/0, setA/3, getA/2]).
 
-%%selectionS(Array,Von,Bis) -> ok.
+%% Von = Index from which you want to start sorting
+%% Bis = Index which shall be the last sorted
+selectionS(Array, Von, Bis) ->
+  selectionS(Array, Von, Bis, initA()).
+  %%TODO: falls nicht ganzer Array sortiert werden soll, muss Rest drumherum gepackt werden
+
+selectionS(Array, Von, Bis, SortedArray) ->
+  SortLength = Bis - Von + 1,
+  Beginning = Von,
+  Minimum = Beginning,
+  if
+
+  end
+
+%% prozedur SelectionSort( A : Liste sortierbarer Elemente )
+%% n = Länge( A )
+%% links = 0
+%% wiederhole
+%% min = links
+%% für jedes i von links + 1 bis n wiederhole
+%% falls A[ i ] < A[ min ] dann
+%% min = i
+%% ende falls
+%% ende für
+%% Vertausche A[ min ] und A[ links ]
+%% links = links + 1
+%% solange links < n
+%% prozedur ende
